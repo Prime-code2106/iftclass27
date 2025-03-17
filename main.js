2b033f07c54a930185b5c156a4b4fa33b3fd810b
Onah Emmanuel <onahemmanueluchenna@gmail.com>
2025-03-17 11:09:33 +0100

    pushing the hamburger

--- main.js
+++ main.js
@@ -4,8 +4,20 @@ function redirectToSignUp() {
 
 var hamburger = document.querySelector('.hamburger-menu')
 var mobilee = document.querySelector('.mobileview')
+var bars = document.getElementsByClassName('menubars')
 
 function showclass() {
     hamburger.classList.toggle('show')
-    mobilee.classList.toggle('mobilesection')
+    mobilee.style.visibility = 'visible';
+    
+    
 }
+
+bars.addEventListener('click', removeclass = () => {
+    if(mobilee.style.visibility == 'visible') {
+        mobilee.style.visibility = 'hidden'
+    } else {
+        mobilee.style.visibility = 'visible';
+    }
+    
+})
